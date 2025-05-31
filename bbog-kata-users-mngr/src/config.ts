57188638a47ef7d8066ea69a2991f84879952bea
Kevin Andrés Simanca Herrera <ksimanc@bancodebogota.com.co>
2025-05-31 13:35:15 -0500

    add environment configuration and update docker-compose for database service

--- bbog-kata-users-mngr/src/config.ts
+++ bbog-kata-users-mngr/src/config.ts
@@ -1,3 +1,12 @@
+import process from 'process';
+
 export default {
   PORT: process.env.APPLICATION_PORT ?? '3000',
+  database: {
+    HOST: process.env.DB_HOST ?? 'localhost',
+    PORT: parseInt(process.env.DB_PORT ?? '5432', 10),
+    NAME: process.env.DB_NAME ?? 'usersdb',
+    USER: process.env.DB_USER ?? 'admin',
+    PASSWORD: process.env.DB_PASSWORD ?? 'admin123',
+  },
 };
