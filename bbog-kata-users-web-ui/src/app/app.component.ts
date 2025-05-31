05ed5edbd5faa005fb46dc15f9dd5b5b6385a817
Kevin Andrés Simanca Herrera <ksimanc@bancodebogota.com.co>
2025-05-31 13:13:46 -0500

    setup docker for front app and ensure connection with the backend

--- bbog-kata-users-web-ui/src/app/app.component.ts
+++ bbog-kata-users-web-ui/src/app/app.component.ts
@@ -10,4 +10,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'bbog-kata-users-web-ui';
+
+  async getUsers() {
+    const endpoint = 'http://localhost:3000/kata-users-mngr/V1/users';
+
+    const res = await fetch(endpoint);
+    const data = await res.json();
+    console.log(data);
+  }
 }
